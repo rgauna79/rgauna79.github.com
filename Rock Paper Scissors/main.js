f4af35880ccab76919ee1a30696019651ed40c10
rgauna79 <rgauna@gmail.com>
2023-10-12 12:00:46 -0600

    Added styles to Rock, Paper, Scissors

--- Rock Paper Scissors/main.js
+++ Rock Paper Scissors/main.js
@@ -4,9 +4,12 @@ let computerScore = 0;
 let playerBoardScore = document.getElementById("player");
 let computerBoardScore = document.getElementById("computer");
 
+const paper = "./assets/paper.jpeg";
+const rock = "./assets/default.jpeg";
+const scissor = "./assets/scissor.jpeg";
 
 //Select All Player Buttons and Listen the choice submitted
-const buttons = document.querySelectorAll("button.btn-player");
+const buttons = document.querySelectorAll("button.btn");
 buttons.forEach(button => {
   button.addEventListener('click', function(){
     playerTurn(button.value);
@@ -23,15 +26,31 @@ function getComputerChoice() {
     return choices[Math.floor(Math.random() * choices.length)];
 }
 
+//Show Player Selection Image
+function showChoice(choice, player) {
+  const elem =  player === 'player' ? 'playerChoice' : 'computerChoice';
+  const image = document.getElementsByClassName(elem)[0];
+  image.removeAttribute('src');
+  image.style.display="block"; 
+  if (choice === 'Paper') {
+    image.setAttribute('src', paper);
+  } else if (choice === 'Rock') {
+    image.setAttribute('src', rock); 
+  } else {
+    image.setAttribute('src', scissor);
+  }
+}
+
+
 
 function playerTurn(playerChoice) {
     let computerSelection = getComputerChoice();
-    showComputerChoice.innerHTML = computerSelection;
-    showWinnerRound = document.getElementById("winner");
-    document.getElementById("playerChoice").innerHTML = playerChoice;
+    showChoice(computerSelection, "computer");  
+    let showWinnerRound = document.getElementById("winner");
+    showChoice(playerChoice, "player");
     let result = '';
 
-    //Check if Player or Computer Win Round
+  //Check if Player or Computer Win Round
     if ((playerChoice === 'Paper' && computerSelection === 'Rock') ||
        (playerChoice === 'Scissors' && computerSelection === 'Paper') ||
        (playerChoice === 'Rock' && computerSelection === 'Scissors')){
@@ -41,7 +60,9 @@ function playerTurn(playerChoice) {
       
         if (playerScore === 5 ){
             showWinnerRound.innerHTML = "Player win game";
-            alert('Player win');
+            setTimeout(function() {
+                alert('Player win');
+            }, 100);
             resetScore();
         }
     } else if (playerChoice === computerSelection) {
@@ -53,7 +74,9 @@ function playerTurn(playerChoice) {
       
         if (computerScore === 5 ){
             showWinnerRound.innerHTML = "Computer win game ";
-            alert('Computer win');
+            setTimeout(function() {
+                alert('Computer win');
+            }, 100);
             resetScore();
         }    
     }
